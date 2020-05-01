91c04e302d2089a699955a3680a9638ceb6b2fe8
alifsultan <alifsultan22@gmail.com>
2020-05-01 16:00:31 +0700

    pertemuan13
    belum fix

---
+++ pertemuan13/src/config/Config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyADGRcfCbeq0VGNSdwYvCapwB58FXVCXew",
+  authDomain: "pertemuan13-9abeb.firebaseapp.com",
+  databaseURL: "https://pertemuan13-9abeb.firebaseio.com",
+  projectId: "pertemuan13-9abeb",
+  storageBucket: "pertemuan13-9abeb.appspot.com",
+  messagingSenderId: "949202739246",
+  appId: "1:949202739246:web:1871ed6cd7c559367f7fbf",
+  measurementId: "G-PFJQ5P26QV"
+};
